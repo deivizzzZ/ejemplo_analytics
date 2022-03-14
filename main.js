95fe970454d46ebd08feb24ec6d50a1ebfabc7da
David Alvarez <94860520+deivizzzZ@users.noreply.github.com>
2022-03-14 12:22:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ANALYTICS CUSTOM DIMENSIONS
-var dimensionValue = 'PAGINA_PRINCIPAL';
-ga('set', 'pageType', dimensionValue);
+var pageType = 'PAGINA_PRINCIPAL';
+ga('set', 'dimension1', pageType);
 
 let clicksCounter = 0;
 function increaseCounter() {
