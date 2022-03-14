9f0dc4c7db26ac62ae85a446bb6326e74990c077
David Alvarez <94860520+deivizzzZ@users.noreply.github.com>
2022-03-14 17:45:35 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ function increaseCounter() {
     document.getElementById("clicks-number").value = clicksCounter;
 }
 function sendClicks() {
-    gtag('event', 'clicks_sent', {'send_to': 'UA-222598052-1', 'clicks_number': clicksCounter});
+    gtag('event', 'clicks_sent', {'clicks_number': clicksCounter});
 }
 
 function sendForm() {
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
-    gtag('event', 'form_sent', {'send_to': 'G-ZP7GPD96M8', 'form_name': `${fname} ${lname}`});
+    gtag('event', 'form_sent', {'form_name': `${fname} ${lname}`});
 }
 
 document.querySelectorAll("a").forEach(link => {
     link.addEventListener('click', () => {
-        gtag('event', 'link_clicked', {'send_to': 'G-ZP7GPD96M8', 'link_visited': link.innerText});
+        gtag('event', 'link_clicked', {'link_visited': link.innerText});
     });
 });
