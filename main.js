8f1b9253f7219f3c00a708db3f64ccb39e0ad38c
David Alvarez <94860520+deivizzzZ@users.noreply.github.com>
2022-03-14 15:23:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-
 let clicksCounter = 0;
 function increaseCounter() {
     clicksCounter++;
     document.querySelector(".counter p").innerText = `Número de clicks: ${clicksCounter}`;
+}
+
+function sendForm() {
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    gtag('event', 'form_sent', {'form_name': `${fname} ${lname}`});
 }
