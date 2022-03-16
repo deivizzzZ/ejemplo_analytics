04be44c6f77b3266b674b00e6bcef866a63bc4d6
David Alvarez <94860520+deivizzzZ@users.noreply.github.com>
2022-03-16 13:34:48 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ function increaseCounter() {
     document.getElementById("clicks-number").value = clicksCounter;
 }
 function sendClicks() {
-    dataLayer.push({
-        'event': 'clicks_sent',
+    gtag('event', 'clicks_sent', {
         'clicks_number': document.getElementById("clicks-number").value
     });
 }
@@ -13,18 +12,11 @@ function sendClicks() {
 function sendForm() {
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
-    dataLayer.push({
-        'event': 'form_sent',
-        'form_name': `${fname} ${lname}`
-    });
+    gtag('event', 'form_sent', {'form_name': `${fname} ${lname}`});
 }
 
-// GTM incorpora un evento de tipo 'linkClick' que efectúa la misma labor
 document.querySelectorAll("a").forEach(link => {
     link.addEventListener('click', () => {
-        dataLayer.push({
-            'event': 'link_clicked',
-            'link_visited': link.innerText
-        });
+        gtag('event', 'link_clicked', {'link_visited': link.innerText});
     });
 });
